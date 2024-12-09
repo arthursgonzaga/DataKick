16d1f0b3ff2c49c8680ebf7f3554cfdf36d572c2
Arthur Gonzaga <arthursgonzaga@gmail.com>
2024-12-09 07:30:09 -0300

    feat: criando estrutura da DAG

---
+++ airflow/dags/silver/manual_data/manual_data.py
@@ -0,0 +1,86 @@
+from datetime import datetime
+from airflow.decorators import dag, task
+from airflow.hooks.base import BaseHook
+import duckdb
+import json
+import yaml
+import os
+
+
+def _duckdb_config(conn_duckdb):
+    connection = BaseHook.get_connection("minio_default")
+    minio_config = json.loads(connection.extra)
+    
+    MINIO_ACCESS_KEY = minio_config.get('aws_access_key_id')
+    MINIO_SECRET_KEY = minio_config.get('aws_secret_access_key')
+    MINIO_ENDPOINT = minio_config.get('endpoint_url')
+
+    config_query = f"""
+        INSTALL httpfs;
+        LOAD httpfs;
+        SET s3_access_key_id='{MINIO_ACCESS_KEY}';
+        SET s3_secret_access_key='{MINIO_SECRET_KEY}';
+        SET s3_endpoint='{MINIO_ENDPOINT}'; -- Endpoint do MinIO
+        SET s3_url_style = 'path';
+        SET s3_use_ssl=false;
+        """
+    
+    try:
+        print('Executing configuration setup:')
+        conn_duckdb.execute(config_query)
+    except:
+        print('Problem with setup configuration in DuckDB')
+
+def read_table_config(file_path='config.yaml'):
+    try:
+        with open(file_path, 'r') as file:
+            config = yaml.safe_load(file)
+        return config
+    except FileNotFoundError:
+        print(f"Erro: Arquivo de configuração '{file_path}' não encontrado.")
+    except yaml.YAMLError as e:
+        print(f"Erro ao processar o arquivo YAML: {e}")
+
+def load_query(dag_path, table_name):
+    query_path = os.path.join(f"{dag_path}/queries", f"{table_name}.sql")
+    try:
+        with open(query_path, 'r') as file:
+            query = file.read()
+        return query
+    except FileNotFoundError:
+        print(f"Erro: Arquivo de query '{query_path}' não encontrado.")
+        return None
+
+@dag(
+    dag_id="manual_data_transform",
+    schedule_interval="0 0 * * *",
+    start_date=datetime(2024, 11, 10),
+    catchup=False,
+    default_args={
+        "retries": 3,
+    },
+    tags=["MANUAL_DATA", "TRANSFORMATION", "SILVER"],
+)
+
+def manual_data_transform():
+
+    dag_path = "/opt/airflow/dags/silver/manual_data"
+    config = read_table_config(f"{dag_path}/config.yaml")
+    tables = config.get('tables', {})
+    for key, value in tables.items():
+        destination_table_name = key
+        source_table_name = value.get('table_name')
+        source_file_name = value.get('file_name')      
+        query = load_query(dag_path, destination_table_name)
+
+        @task(task_id=f"transform_{destination_table_name}")
+        def transform_data(source_table_name, source_file_name, destination_table_name, query):
+            print(f"Processing table: {destination_table_name}")
+            print(f"Query: {query}")
+            print(f"Source Table: {source_table_name}, Source File: {source_file_name}")
+
+
+
+        transform_data(source_table_name, source_file_name, destination_table_name, query)
+
+manual_data_transform()
